8cf57b4caa29fae90e1e99e9800e1057e9ad20e3
gingko098 <fjummadil@gmail.com>
2022-01-26 21:39:52 +0700

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,415 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.txtNPWP = New System.Windows.Forms.TextBox()
+        Me.txtNAMA = New System.Windows.Forms.TextBox()
+        Me.txtALAMAT = New System.Windows.Forms.TextBox()
+        Me.txtGAJISEBULAN = New System.Windows.Forms.TextBox()
+        Me.txtASURANSIKECELAKAAN = New System.Windows.Forms.TextBox()
+        Me.txtASURANSIKEMATIAN = New System.Windows.Forms.TextBox()
+        Me.txtASURANSIPENSIUN = New System.Windows.Forms.TextBox()
+        Me.txtGAJIBRUTO = New System.Windows.Forms.TextBox()
+        Me.txtPAJAK = New System.Windows.Forms.TextBox()
+        Me.txtTOTAL = New System.Windows.Forms.TextBox()
+        Me.txtPAJAKBULAN = New System.Windows.Forms.TextBox()
+        Me.txtPAJAKTAHUN = New System.Windows.Forms.TextBox()
+        Me.cmdhasil = New System.Windows.Forms.Button()
+        Me.cmdhapus = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(147, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(549, 44)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Direktorat Jenderal Pajak Kementrian Keuangan Negara RI" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "                        " & _
+            "     Pajak Penghasilan PPH"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(9, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(324, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Bagian A. Identitas Pemungut Pajak Wajib Pajak"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(167, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "NPWP"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(164, 163)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Nama" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(154, 188)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(55, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Alamat" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(9, 202)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(151, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Bagian B. Objek Pajak" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(226, 247)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(90, 15)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Gaji Sebulan"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.Transparent
+        Me.Label8.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(122, 270)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(202, 15)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Asuransi Jaminan Kecelakaan"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.Color.Transparent
+        Me.Label9.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(188, 293)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(132, 15)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Asuransi Kematian"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.Color.Transparent
+        Me.Label10.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(196, 316)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(118, 15)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "Asuransi Pensiun"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.BackColor = System.Drawing.Color.Transparent
+        Me.Label11.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(215, 365)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(124, 15)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "Jumlah Gaji Bruto"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.BackColor = System.Drawing.Color.Transparent
+        Me.Label12.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(194, 391)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(147, 15)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "Pajak Penghasilan 5%"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.BackColor = System.Drawing.Color.Transparent
+        Me.Label13.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(148, 420)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(193, 15)
+        Me.Label13.TabIndex = 12
+        Me.Label13.Text = "Total Yang Dibayarkan   Rp"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.BackColor = System.Drawing.Color.Transparent
+        Me.Label14.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(128, 461)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(213, 15)
+        Me.Label14.TabIndex = 13
+        Me.Label14.Text = "Pajak Penghasilan 1 Bulan (PPH)"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.BackColor = System.Drawing.Color.Transparent
+        Me.Label15.Font = New System.Drawing.Font("Lucida Sans", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(88, 490)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(256, 15)
+        Me.Label15.TabIndex = 14
+        Me.Label15.Text = "Jumlah PPH Selama 1 Tahun (12 Bulan)"
+        '
+        'txtNPWP
+        '
+        Me.txtNPWP.Location = New System.Drawing.Point(218, 131)
+        Me.txtNPWP.Name = "txtNPWP"
+        Me.txtNPWP.Size = New System.Drawing.Size(115, 20)
+        Me.txtNPWP.TabIndex = 15
+        '
+        'txtNAMA
+        '
+        Me.txtNAMA.Location = New System.Drawing.Point(218, 157)
+        Me.txtNAMA.Name = "txtNAMA"
+        Me.txtNAMA.Size = New System.Drawing.Size(176, 20)
+        Me.txtNAMA.TabIndex = 16
+        '
+        'txtALAMAT
+        '
+        Me.txtALAMAT.Location = New System.Drawing.Point(218, 183)
+        Me.txtALAMAT.Name = "txtALAMAT"
+        Me.txtALAMAT.Size = New System.Drawing.Size(176, 20)
+        Me.txtALAMAT.TabIndex = 17
+        '
+        'txtGAJISEBULAN
+        '
+        Me.txtGAJISEBULAN.Location = New System.Drawing.Point(316, 242)
+        Me.txtGAJISEBULAN.Name = "txtGAJISEBULAN"
+        Me.txtGAJISEBULAN.Size = New System.Drawing.Size(195, 20)
+        Me.txtGAJISEBULAN.TabIndex = 18
+        '
+        'txtASURANSIKECELAKAAN
+        '
+        Me.txtASURANSIKECELAKAAN.Location = New System.Drawing.Point(316, 265)
+        Me.txtASURANSIKECELAKAAN.Name = "txtASURANSIKECELAKAAN"
+        Me.txtASURANSIKECELAKAAN.Size = New System.Drawing.Size(195, 20)
+        Me.txtASURANSIKECELAKAAN.TabIndex = 19
+        '
+        'txtASURANSIKEMATIAN
+        '
+        Me.txtASURANSIKEMATIAN.Location = New System.Drawing.Point(316, 288)
+        Me.txtASURANSIKEMATIAN.Name = "txtASURANSIKEMATIAN"
+        Me.txtASURANSIKEMATIAN.Size = New System.Drawing.Size(195, 20)
+        Me.txtASURANSIKEMATIAN.TabIndex = 20
+        '
+        'txtASURANSIPENSIUN
+        '
+        Me.txtASURANSIPENSIUN.Location = New System.Drawing.Point(316, 311)
+        Me.txtASURANSIPENSIUN.Name = "txtASURANSIPENSIUN"
+        Me.txtASURANSIPENSIUN.Size = New System.Drawing.Size(195, 20)
+        Me.txtASURANSIPENSIUN.TabIndex = 21
+        '
+        'txtGAJIBRUTO
+        '
+        Me.txtGAJIBRUTO.Location = New System.Drawing.Point(396, 360)
+        Me.txtGAJIBRUTO.Name = "txtGAJIBRUTO"
+        Me.txtGAJIBRUTO.Size = New System.Drawing.Size(147, 20)
+        Me.txtGAJIBRUTO.TabIndex = 22
+        '
+        'txtPAJAK
+        '
+        Me.txtPAJAK.Location = New System.Drawing.Point(396, 386)
+        Me.txtPAJAK.Name = "txtPAJAK"
+        Me.txtPAJAK.Size = New System.Drawing.Size(147, 20)
+        Me.txtPAJAK.TabIndex = 23
+        '
+        'txtTOTAL
+        '
+        Me.txtTOTAL.Location = New System.Drawing.Point(396, 415)
+        Me.txtTOTAL.Name = "txtTOTAL"
+        Me.txtTOTAL.Size = New System.Drawing.Size(147, 20)
+        Me.txtTOTAL.TabIndex = 24
+        '
+        'txtPAJAKBULAN
+        '
+        Me.txtPAJAKBULAN.Location = New System.Drawing.Point(350, 456)
+        Me.txtPAJAKBULAN.Name = "txtPAJAKBULAN"
+        Me.txtPAJAKBULAN.Size = New System.Drawing.Size(182, 20)
+        Me.txtPAJAKBULAN.TabIndex = 25
+        '
+        'txtPAJAKTAHUN
+        '
+        Me.txtPAJAKTAHUN.Location = New System.Drawing.Point(350, 485)
+        Me.txtPAJAKTAHUN.Name = "txtPAJAKTAHUN"
+        Me.txtPAJAKTAHUN.Size = New System.Drawing.Size(182, 20)
+        Me.txtPAJAKTAHUN.TabIndex = 26
+        '
+        'cmdhasil
+        '
+        Me.cmdhasil.Location = New System.Drawing.Point(527, 319)
+        Me.cmdhasil.Name = "cmdhasil"
+        Me.cmdhasil.Size = New System.Drawing.Size(76, 35)
+        Me.cmdhasil.TabIndex = 27
+        Me.cmdhasil.Text = "Proses"
+        Me.cmdhasil.UseVisualStyleBackColor = True
+        '
+        'cmdhapus
+        '
+        Me.cmdhapus.Location = New System.Drawing.Point(647, 470)
+        Me.cmdhapus.Name = "cmdhapus"
+        Me.cmdhapus.Size = New System.Drawing.Size(49, 35)
+        Me.cmdhapus.TabIndex = 28
+        Me.cmdhapus.Text = "DEL"
+        Me.cmdhapus.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(51, 2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(90, 99)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 29
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(766, 538)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.cmdhapus)
+        Me.Controls.Add(Me.cmdhasil)
+        Me.Controls.Add(Me.txtPAJAKTAHUN)
+        Me.Controls.Add(Me.txtPAJAKBULAN)
+        Me.Controls.Add(Me.txtTOTAL)
+        Me.Controls.Add(Me.txtPAJAK)
+        Me.Controls.Add(Me.txtGAJIBRUTO)
+        Me.Controls.Add(Me.txtASURANSIPENSIUN)
+        Me.Controls.Add(Me.txtASURANSIKEMATIAN)
+        Me.Controls.Add(Me.txtASURANSIKECELAKAAN)
+        Me.Controls.Add(Me.txtGAJISEBULAN)
+        Me.Controls.Add(Me.txtALAMAT)
+        Me.Controls.Add(Me.txtNAMA)
+        Me.Controls.Add(Me.txtNPWP)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "PAJAK"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents txtNPWP As System.Windows.Forms.TextBox
+    Friend WithEvents txtNAMA As System.Windows.Forms.TextBox
+    Friend WithEvents txtALAMAT As System.Windows.Forms.TextBox
+    Friend WithEvents txtGAJISEBULAN As System.Windows.Forms.TextBox
+    Friend WithEvents txtASURANSIKECELAKAAN As System.Windows.Forms.TextBox
+    Friend WithEvents txtASURANSIKEMATIAN As System.Windows.Forms.TextBox
+    Friend WithEvents txtASURANSIPENSIUN As System.Windows.Forms.TextBox
+    Friend WithEvents txtGAJIBRUTO As System.Windows.Forms.TextBox
+    Friend WithEvents txtPAJAK As System.Windows.Forms.TextBox
+    Friend WithEvents txtTOTAL As System.Windows.Forms.TextBox
+    Friend WithEvents txtPAJAKBULAN As System.Windows.Forms.TextBox
+    Friend WithEvents txtPAJAKTAHUN As System.Windows.Forms.TextBox
+    Friend WithEvents cmdhasil As System.Windows.Forms.Button
+    Friend WithEvents cmdhapus As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub cmdhasil_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdhasil.Click
+        Const penghasilan As Double = 0.05
+        Dim harga As Single
+
+        harga = txtGAJIBRUTO.Text
+        txtPAJAK.Text = 0.05 * txtGAJIBRUTO.Text
+        txtTOTAL.Text = txtGAJIBRUTO.Text * penghasilan
+        txtPAJAKBULAN.Text = penghasilan
+        txtPAJAKTAHUN.Text = txtPAJAKBULAN.Text * 12
+
+    End Sub
+
+
+    Private Sub txtASURANSIPENSIUN_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtASURANSIPENSIUN.TextChanged
+        txtGAJIBRUTO.Text = Val(txtGAJISEBULAN.Text) + Val(txtASURANSIKECELAKAAN.Text) + Val(txtASURANSIKEMATIAN.Text) + Val(txtASURANSIPENSIUN.Text)
+    End Sub
+
+    Private Sub cmdhapus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdhapus.Click
+        MsgBox("Hapus data")
+        txtALAMAT.Text = ""
+        txtNPWP.Text = ""
+        txtNAMA.Text = ""
+        txtGAJISEBULAN.Text = ""
+        txtASURANSIKECELAKAAN.Text = ""
+        txtASURANSIKEMATIAN.Text = ""
+        txtASURANSIPENSIUN.Text = ""
+        txtGAJIBRUTO.Text = ""
+        txtPAJAK.Text = ""
+        txtTOTAL.Text = ""
+        txtPAJAKBULAN.Text = ""
+        txtPAJAKTAHUN.Text = ""
+    End Sub
+End Class
+
